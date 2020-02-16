be35d2c3903112839b2a9b5714b808490dba9eb2
Shayan Aslani <shayan.aslany@yahoo.com>
2020-02-16 20:20:12 +0330

    Create Single Fragment Activity

--- app/src/main/java/com/shayanaslani/foursquareexample/view/activity/MainActivity.java
+++ app/src/main/java/com/shayanaslani/foursquareexample/view/activity/MainActivity.java
@@ -1,16 +1,23 @@
 package com.shayanaslani.foursquareexample.view.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
 import com.shayanaslani.foursquareexample.R;
+import com.shayanaslani.foursquareexample.view.fragment.PlaceListFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends SingleFragmentActivity {
+
+    @Override
+    public Fragment createFragment() {
+        return PlaceListFragment.newInstance();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_single_fragment);
     }
 }
---
+++ app/src/main/java/com/shayanaslani/foursquareexample/view/activity/SingleFragmentActivity.java
@@ -0,0 +1,31 @@
+package com.shayanaslani.foursquareexample.view.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.shayanaslani.foursquareexample.R;
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    public abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_fragment);
+
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+        if (fragment == null)
+            fragmentManager
+                    .beginTransaction()
+                    .add(R.id.fragment_container, createFragment())
+                    .commit();
+
+    }
+}
+
