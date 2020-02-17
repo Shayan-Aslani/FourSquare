d551daea56c90ec48981bc2db4be7939036e0512
Shayan Aslani <shayan.aslany@yahoo.com>
2020-02-17 19:22:07 +0330

    Add check and request loaction permissions .

--- app/src/main/java/com/shayanaslani/foursquareexample/view/activity/MainActivity.java
+++ app/src/main/java/com/shayanaslani/foursquareexample/view/activity/MainActivity.java
@@ -3,7 +3,6 @@ package com.shayanaslani.foursquareexample.view.activity;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
-
 import com.shayanaslani.foursquareexample.R;
 import com.shayanaslani.foursquareexample.view.fragment.VenueListFragment;
 
@@ -18,5 +17,6 @@ public class MainActivity extends SingleFragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_fragment);
+
     }
 }
--- app/src/main/java/com/shayanaslani/foursquareexample/view/fragment/VenueListFragment.java
+++ app/src/main/java/com/shayanaslani/foursquareexample/view/fragment/VenueListFragment.java
@@ -1,29 +1,36 @@
 package com.shayanaslani.foursquareexample.view.fragment;
 
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.model.LatLng;
 import com.shayanaslani.foursquareexample.R;
-import com.shayanaslani.foursquareexample.VenueRepository;
 import com.shayanaslani.foursquareexample.adapters.VenueItemsAdapter;
 import com.shayanaslani.foursquareexample.databinding.FragmentVenueListBinding;
 import com.shayanaslani.foursquareexample.viewmodel.VenueListFragmentViewModel;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static com.shayanaslani.foursquareexample.model.FourSquareJSON.*;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 
 /**
@@ -31,9 +38,11 @@ import static com.shayanaslani.foursquareexample.model.FourSquareJSON.*;
  */
 public class VenueListFragment extends Fragment {
 
-    private FragmentVenueListBinding mBinding ;
-    private VenueListFragmentViewModel mViewModel ;
-    private VenueItemsAdapter venueItemsAdapter ;
+    public static final int PERMISSION_ID = 100;
+    private FragmentVenueListBinding mBinding;
+    private VenueListFragmentViewModel mViewModel;
+
+    private VenueItemsAdapter venueItemsAdapter;
 
     public static VenueListFragment newInstance() {
 
@@ -52,30 +61,85 @@ public class VenueListFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mViewModel = ViewModelProviders.of(getActivity()).get(VenueListFragmentViewModel.class);
+        if (!checkPermissions()) {
+            requestPermission();
+        }
+        if (!isLocationEnabled()) {
+            requestLocation();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_ID) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getContext(), "permissin", Toast.LENGTH_SHORT).show();
+            } else if (shouldShowRequestPermissionRationale(ACCESS_FINE_LOCATION)) {
+                new AlertDialog.Builder(getActivity())
+                        .setMessage("You need to allow access to both the permissions")
+                        .setPositiveButton("OK", (dialogInterface, i) -> requestPermission())
+                        .setNegativeButton("Cancel", null)
+                        .create()
+                        .show();
+            }
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        mBinding = DataBindingUtil.inflate(inflater , R.layout.fragment_venue_list, container, false);
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_venue_list, container, false);
 
         setupRecyclerView();
 
-        mViewModel.loadVenueListFromApi(new LatLng(35.69,51.40));
+        mViewModel.loadVenueListFromApi(new LatLng(35.69, 51.40));
 
-        mViewModel.getVenueItemsLiveData().observe(this , items -> {
+        mViewModel.getVenueItemsLiveData().observe(this, items -> {
             venueItemsAdapter.setVenueItemsList(items);
         });
-
 
         return mBinding.getRoot();
     }
 
-    private void setupRecyclerView(){
+    private void setupRecyclerView() {
         venueItemsAdapter = new VenueItemsAdapter(getContext());
         mBinding.placeListRv.setLayoutManager(new LinearLayoutManager(getContext()));
         mBinding.placeListRv.setAdapter(venueItemsAdapter);
     }
 
+    private boolean checkPermissions() {
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }
+        return false;
+    }
+
+    private void requestPermission() {
+        requestPermissions(
+                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
+                PERMISSION_ID
+        );
+    }
+
+    private boolean isLocationEnabled() {
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+                LocationManager.NETWORK_PROVIDER
+        );
+    }
+
+    private void requestLocation() {
+        new AlertDialog.Builder(getActivity())
+                .setMessage("You need to turn on location")
+                .setPositiveButton("Turn on", (dialogInterface, i) -> {
+                            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                            startActivity(intent);
+                        })
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
 }
